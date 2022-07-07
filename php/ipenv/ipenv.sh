47ec5dbf7a11e5a76cdceb63d77b57962daf705b
jr-k <jessym@santeacademie.com>
2022-07-07 16:17:06 +0200

    update notice

--- php/ipenv/ipenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ip=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | grep 192.168 | cut -d\  -f2)
-sed -E -i '' "s/192.168.[0-9][0-9]?[0-9]?.[0-9][0-9]?[0-9]?/$ip/g" contentplayer-mobile/.env
-echo "👻 Updated ip address on env file : $ip"
-
